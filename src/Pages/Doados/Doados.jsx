5e5dc542625f1ec910e21ee9fabdc2e6d0dab2a5
Lucasvpl <lucasvpldeveloper@gmail.com>
2024-12-02 19:40:19 -0300

    Criei a tela responsiva da página de inicio

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,18 @@
-
-
+import S from "./doados.module.scss"
+import protagonista from "../../assets/protagonista.png"
 
 export default function Doados() {
     return(
-        <section>
-            <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+                <section className={S.boxBook}>
+                    <article>
+                        <img src={protagonista} alt="Imagens do livro o protagonista" />
+                        <h3>O Protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </article>
+                </section>
         </section>
     )
 }
